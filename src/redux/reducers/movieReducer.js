8d7a6c0d854ba535922eb925f965dc8273b6c2ab
alqamah <alqama.hasnain@gmail.com>
2024-06-25 10:24:50 +0530

    initialised the dir structure and redux-store

---
+++ src/redux/reducers/movieReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  movies: []
+};
+
+const movieReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // We'll add cases here later
+    default:
+      return state;
+  }
+};
+
+export default movieReducer;
